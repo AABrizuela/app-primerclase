89c2870957608b58a5e8b527565d640da38d317e
Adrian Brizuela <adrianalebrizuela@gmail.com>
2020-08-27 22:51:16 -0300

    Tercer Commit, punto 1 termina2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app-primerclase';
+
+  public edadUno:number = null;
+  public edadDos:number = null;
+  public suma:number = 0;
+  public promedio:number = 0;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  calcular() {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma/2;
+  }
+
+  limpiar(){
+    this.edadUno = null;
+    this.edadDos = null;
+    this.suma = 0;
+    this.promedio = 0;
+  }
 }
